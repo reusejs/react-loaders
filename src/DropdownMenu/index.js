dab3290682b1abb9a9028f66acd4f1fc20bacae8
Rajiv Seelam <rajiv@betalectic.com>
2021-11-02 15:47:31 +0530

    Add hover color

--- src/DropdownMenu/index.js
+++ src/DropdownMenu/index.js
@@ -7,17 +7,13 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-const DropdownMenu = function ({ profilePictureUrl, teams }) {
+const DropdownMenu = function ({ label, sections, ...props }) {
   return (
-    <Menu as="div" className="ml-3 relative">
+    <Menu as="div" className="relative">
       <div>
-        <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-400 focus:ring-white">
+        <Menu.Button className="flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-400 focus:ring-white dark:focus:ring-gray-900">
           <span className="sr-only">Open user menu</span>
-          <img
-            className="h-8 w-8 rounded-full"
-            src={profilePictureUrl}
-            alt=""
-          />
+          <>{label}</>
         </Menu.Button>
       </div>
       <Transition
@@ -29,123 +25,55 @@ const DropdownMenu = function ({ profilePictureUrl, teams }) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
-          <div className="py-1">
-            <section>
-              <div>
-                <div className="block px-4 py-2 text-xs text-gray-500">
-                  Manage Account
-                </div>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={classNames(
-                        active ? "bg-gray-100" : "",
-                        "block px-4 py-2 text-sm text-gray-700"
-                      )}
+        <Menu.Items
+          className={classNames(
+            props.positionClasses || "origin-top-right absolute right-0",
+            "mt-2 w-48 rounded-md shadow-lg py-1 bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 dark:divide-gray-500 focus:outline-none"
+          )}
+        >
+          {sections.map((section, sectionIndex) => (
+            <div className="py-1" key={`section-${sectionIndex}`}>
+              <section>
+                <div>
+                  {section.label && (
+                    <div
+                      className={
+                        props.sectionLabelClasses ||
+                        "block px-4 py-2 text-xs text-gray-500 dark:text-gray-300"
+                      }
                     >
-                      Your Profile
-                    </a>
+                      {section.label}
+                    </div>
                   )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={classNames(
-                        active ? "bg-gray-100" : "",
-                        "block px-4 py-2 text-sm text-gray-700"
-                      )}
+                  {section.items.map((item, itemIndex) => (
+                    <Menu.Item
+                      key={`section-${sectionIndex}-item-${itemIndex}`}
                     >
-                      Settings
-                    </a>
-                  )}
-                </Menu.Item>
-              </div>
-            </section>
-          </div>
-          <div className="py-1">
-            <section>
-              <div>
-                <div className="block px-4 py-2 text-xs text-gray-500">
-                  Manage Teams
-                </div>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={classNames(
-                        active ? "bg-gray-100" : "",
-                        "block px-4 py-2 text-sm text-gray-700"
-                      )}
-                    >
-                      Teams Settings
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={classNames(
-                        active ? "bg-gray-100" : "",
-                        "block px-4 py-2 text-sm text-gray-700"
-                      )}
-                    >
-                      Create New Team
-                    </a>
-                  )}
-                </Menu.Item>
-              </div>
-            </section>
-          </div>
-          <div className="py-1">
-            <section>
-              <div>
-                <div className="block px-4 py-2 text-xs text-gray-500">
-                  Switch Teams
-                </div>
-                {teams.map((item, index) => (
-                  <Menu.Item key={index}>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames(
-                          active ? "bg-gray-100" : "",
-                          "block px-4 py-2 text-sm text-gray-700"
-                        )}
-                      >
-                        {item.current ? (
-                          <div className="flex items-center">
-                            <CheckCircleIcon className="text-green-500 h-6 w-6 mr-1" />
-                            {item.name}{" "}
+                      {({}) => (
+                        <a
+                          href="#"
+                          className={classNames(
+                            item.active
+                              ? props.activeItemClasses ||
+                                  "bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-white"
+                              : "",
+                            "block px-4 py-2 text-sm text-gray-700 dark:text-gray-100 hover:bg-gray-50 dark:hover:bg-gray-700"
+                          )}
+                        >
+                          <div className="flex items-center justify-between">
+                            <div>{item.label}</div>
+                            {item.active && section.activeIcon && (
+                              <>{section.activeIcon}</>
+                            )}
                           </div>
-                        ) : (
-                          item.name
-                        )}
-                      </a>
-                    )}
-                  </Menu.Item>
-                ))}
-              </div>
-            </section>
-          </div>
-          <div className="py-1">
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? "bg-gray-100" : "",
-                    "block px-4 py-2 text-sm text-gray-700"
-                  )}
-                >
-                  Sign out
-                </a>
-              )}
-            </Menu.Item>
-          </div>
+                        </a>
+                      )}
+                    </Menu.Item>
+                  ))}
+                </div>
+              </section>
+            </div>
+          ))}
         </Menu.Items>
       </Transition>
     </Menu>
